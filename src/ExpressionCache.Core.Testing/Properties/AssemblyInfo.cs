d15369ea21df24d6665c6e5dcecdd7442ce2dab1
Morten Larsen <baunegaard@live.dk>
2021-01-20 01:58:31 +0100

    Make GitVersion handle assembly version

---
+++ src/ExpressionCache.Core.Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+// These attributes will be patched by GitVersion
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
---
+++ src/ExpressionCache.Distributed/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Reflection;
+
+[assembly:CLSCompliant(true)]
+
+// These attributes will be patched by GitVersion
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
