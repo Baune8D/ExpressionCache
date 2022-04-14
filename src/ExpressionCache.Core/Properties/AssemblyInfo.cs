756c9d0b338ad1a97e5ca136854f8230d2aabbc4
Morten Larsen <baunegaard@live.dk>
2022-04-14 18:06:21 +0200

    Fix warnings by marking as not CLS compliant

--- src/ExpressionCache.Core/Properties/AssemblyInfo.cs
+++ src/ExpressionCache.Core/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Reflection;
 
-[assembly:CLSCompliant(true)]
+[assembly:CLSCompliant(false)]
 
 // These attributes will be patched by GitVersion
 [assembly: AssemblyVersion("1.0.0.0")]
